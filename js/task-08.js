b33c89619c64acb307898655151af842a0057de4
ArtemHryn <grynbest@gmail.com>
2022-07-15 16:55:52 +0300

    tasks 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const inputFields = event.currentTarget.elements;
+  if (inputFields.email.value && inputFields.password.value === "") {
+    alert("fill in all fields");
+    return;
+  }
+  const email = inputFields.email.value;
+  const password = inputFields.password.value;
+  const formData = { email, password };
+  console.log(formData);
+  return formData;
+}
