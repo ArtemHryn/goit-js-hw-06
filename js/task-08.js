6628c8cfc27c616a8008b097da770b744b28c82b
ArtemHryn <grynbest@gmail.com>
2022-07-28 18:07:27 +0300

    fixed

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,17 @@
-
 const form = document.querySelector(".login-form");
 form.addEventListener("submit", onSubmitForm);
 
 function onSubmitForm(event) {
+  const email = inputFields.email.value;
+  const password = inputFields.password.value;
   event.preventDefault();
   const inputFields = event.currentTarget.elements;
-  if (inputFields.email.value && inputFields.password.value === "") {
+  if (inputFields.email.value || inputFields.password.value === "") {
     alert("fill in all fields");
     return;
   }
-  const email = inputFields.email.value;
-  const password = inputFields.password.value;
-  const formData = { email, password }
-  console.log(formData);
-  event.currentTarget.reset()
-}
 
+  const formData = { email, password };
+  console.log(formData);
+  event.currentTarget.reset();
+}
